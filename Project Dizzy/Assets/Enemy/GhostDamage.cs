2a1841a2d19ab955e2765009a5453722d0e872fb
mattvillaOG <113946048+mattvillaOG@users.noreply.github.com>
2026-02-26 01:11:22 -0500

    Implemented Enemy
    I created a Ghost prefab, that seeks out the player, deals damage to the player in the form of decreasing their spin speed, and when the player's speed is 0 the game is over. Bullets also now take out the ghosts.

---
+++ Project Dizzy/Assets/Enemy/GhostDamage.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GhostDamage : MonoBehaviour
+{
+    [SerializeField] private float damage = 50f;
+    [SerializeField] private string playerTag = "Player";
+
+    public float health = 100f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger entered with: " + collision.name);
+
+        if (collision.CompareTag(playerTag))
+        {
+            SpinMovement player = collision.GetComponent<SpinMovement>();
+
+            if (player != null)
+            {
+                player.TakeDamage(damage);
+                Debug.Log("Damage applied!");
+            }
+            else
+            {
+                Debug.Log("SpinMovement not found!");
+            }
+
+            Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("Projectile"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
