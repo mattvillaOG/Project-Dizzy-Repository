def444a032ca08144d8925d765fab3de82bbb02b
mattvillaOG <113946048+mattvillaOG@users.noreply.github.com>
2026-03-23 19:54:19 -0400

    All done with UI
    I am basically finished with the UI, other than if more bugs come up. The game is looking to ship as soon as possible.

--- Project Dizzy/Assets/User Interface/TitleScript.cs
+++ Project Dizzy/Assets/User Interface/TitleScript.cs
@@ -3,18 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour
 {
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private Animator animator;
+
+    public void PlayMusic()
+    {
+        if (!musicSource.isPlaying)
+            musicSource.Play();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AudioMachine.Instance.PlayMusic("Select Music");
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("OpenStory", false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Trasition() { Debug.Log("close title and open intro."); }
+    public void Trasition() { animator.SetTrigger("PlayTitleScreenB"); Debug.Log("close title and open intro."); }
     public void BeginGame() { SceneManager.LoadScene("LevelSelect"); }
 }
